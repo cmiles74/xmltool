688d4ccc81e2596f8fb2f76c2fd89da7a7199fdc
Christopher Miles <twitch@nervestaple.com>
2020-04-26 16:13:03 -0400

    Worked on Java modules and JavaFX

--- project.clj
+++ project.clj
@@ -1,22 +1,21 @@
 (defproject windsorsolutions/xmltool "1.0"
   :description "A GUI tool for managing and visualizing XML data"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.4.474"]
-                 [org.clojure/tools.cli "0.3.5"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "1.1.587"]
+                 [org.clojure/tools.cli "1.0.194"]
                  [com.taoensso/timbre "4.10.0"]
                  [slingshot "0.12.2"]
-                 [com.nervestaple/javafx "14.0.1"]
-                 ;; [com.nervestaple/javafx-windows-x86-64-native-deps "14.0.1"]
-                 [org.fxmisc.richtext/richtextfx "0.8.2"]]
-  :main windsorsolutions.xmltool.main
-  :launch4j-config-file "dev-resources/launch4j-config.xml"
+                 [org.fxmisc.richtext/richtextfx "0.10.5"]]
+  :plugins [[cmiles74/lein-jlink "0.2.2-SNAPSHOT"]]
   :middleware [leiningen.jlink/middleware]
+  :jlink-jdk-path "C:\\Program Files\\Java\\jdk-14.0.1"
+  :jlink-jre-image true
+  :jlink-jre-image-path "dist/windows64"
   :jlink-module-paths ["C:\\Program Files\\Java\\javafx-jmods-14.0.1"]
   :jlink-modules ["javafx.base" "javafx.controls" "javafx.fxml" "javafx.graphics"
                   "javafx.media""javafx.swing" "javafx.web" "java.sql"]
+  :main windsorsolutions.xmltool.main
   :resource-paths ["resources"]
-  :plugins [[com.nervestaple/lein-jlink "0.2.2-SNAPSHOT"]]
-  :alias {"build" ["do" ["jlink init"] ]}
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["dev"]
                    :resource-paths ["dev-resources"]}})
